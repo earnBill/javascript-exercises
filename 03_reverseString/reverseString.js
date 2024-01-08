ce89fdd16a136d0f7d55e0b2cd729f78be58c041
billTas <billtas@gmail.com>
2024-01-08 22:14:50 +0200

    Finish 1,2,3,4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revercedString = "";
+    for (let i = string.length -1 ; i >= 0; i--) {
+        revercedString += string[i];  
+    }
+    return revercedString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, number) {
+   let newArray = [];
+   for (let items of array) {
+      if (items !== number) {
+         newArray.push(items);
+      }   
+   }
+   return newArray;
+   // return array.splice(number, 1);
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1,2,3,4,5,1,1,1,2,2,1,1], 1));
