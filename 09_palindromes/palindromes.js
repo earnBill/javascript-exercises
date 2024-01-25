6b6df78db26b93f01df0eecdba7a215001731f3b
billTas <billtas@gmail.com>
2024-01-26 01:40:28 +0200

    finish

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reversed = string.split('').reverse().join('').toLowerCase();
+    reversed = reversed.replace(/.,!/g,'1')
 
+    // let reverced = '';
+    // for (let i = string.length - 1; i >= 0 ; i--) {     
+    //     reverced += string[i];
+    // }
+    // if (reverced === string) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+    return reversed;
 };
 
+console.log(palindromes('BilliS!.'));
+// console.log(palindromes("billib"));
 // Do not edit below this line
-module.exports = palindromes;
+
+// module.exports = palindromes;
